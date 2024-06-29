d5f8c84aca6e6b946c77e6c412db0ae77a77f284
3sut2z <145888543+3sut2z@users.noreply.github.com>
2024-06-29 17:55:23 +0700

    Update Anonymous-hub.lua

--- Anonymous-hub.lua
+++ Anonymous-hub.lua
@@ -2536,7 +2536,7 @@ ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable = true
-ImageButton.Image = "http://www.roblox.com/asset/?id=18256954669"
+ImageButton.Image = "http://www.roblox.com/asset/?id=18258697020"
 ImageButton.MouseButton1Down:connect(function()
     game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.End,false,game)
 end)
